4bcea0fa8d9280dd006cf66455ede58668b5fbe1
Liyan Zhao <return65535@qq.com>
2023-08-03 13:45:16 +0800

    init

---
+++ src/main/java/com/github/zly2006/comebackmyvillagers/ComeBackMyVillagers.java
@@ -0,0 +1,10 @@
+package com.github.zly2006.comebackmyvillagers;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ComeBackMyVillagers implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
