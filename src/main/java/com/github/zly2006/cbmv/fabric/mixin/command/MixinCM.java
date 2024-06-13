95c30f563a4c7cf116cce0b4e8317dcb1223fe4e
Liyan Zhao <return65535@qq.com>
2024-06-14 03:55:42 +0800

    fix

--- src/main/java/com/github/zly2006/cbmv/fabric/mixin/command/MixinCM.java
+++ src/main/java/com/github/zly2006/cbmv/fabric/mixin/command/MixinCM.java
@@ -12,6 +12,7 @@ import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -45,6 +46,7 @@ public abstract class MixinCM {
                 })));
     }
 
+    @Unique
     private Command<ServerCommandSource> toggle(String name) {
         return context -> {
             try {
